f90194cb3441a197c086ad4a17f193a4aaea8682
Mexolius <39342674+Mexolius@users.noreply.github.com>
2022-03-28 00:36:05 +0200

    Communication infrastrcuture

---
+++ Client/Query.py
@@ -0,0 +1,55 @@
+import json
+from abc import ABC, abstractmethod
+from typing import Sequence
+
+from RabbitMQClient import RabbitMQProducer
+
+
+class RabbitMQMessage(ABC):
+    def exchange(self):
+        return 'ImageFinder'
+
+    @abstractmethod
+    def queue(self):
+        pass
+
+    def json(self):
+        return json.dumps(self.__dict__)
+
+
+class Query(RabbitMQMessage, ABC):
+    def __init__(self, paths: Sequence[str], params):
+        self.path_number = len(paths)
+        self.paths = paths
+        self.param_number = len(params)
+        self.params = params
+
+
+class ResultResponse(RabbitMQMessage):
+    def __init__(self, result, data, sender):
+        self.result = result
+        self.data = data
+        self.total = len(data)
+        self.sender = sender
+
+    def queue(self):
+        return 'results'
+
+
+class SimpleQuery(Query):
+    def __init__(self, paths: Sequence[str], data, moreData):
+        params = {"data": data, "moreData": moreData}
+        super().__init__(paths, params)
+
+    def queue(self):
+        return 'size'
+
+
+class QueryExecutor:
+    def __init__(self, producer: RabbitMQProducer):
+        self.__producer = producer
+
+    def execute(self, queries: Sequence[Query]):
+        for q in queries:
+            print(f"sent {q.json()} to {q.queue()} @ {q.exchange()}")
+            self.__producer.publish(q.exchange(), q.queue(), q.json())
