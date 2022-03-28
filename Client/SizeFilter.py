c7e25c4c315f9225db6537f179c1db88365316d8
Krzysztof <kczech123kczech123@gmail.com>
2022-03-29 00:52:54 +0200

    fix after review

--- Client/SizeFilter.py
+++ Client/SizeFilter.py
@@ -1,29 +1,29 @@
 import os
 
+
+
 def check_size_in_KB(path):
     size_in_b = os.path.getsize(path)
     size_in_KB = size_in_b/1024
     return size_in_KB
 
-def compare(reference , checked, comparator, threshold = 0):
-    if comparator == "==":
-        return abs(reference - checked) < threshold
-    elif comparator == ">":
-        return checked > reference
-    elif comparator == ">=":
-        return checked >= reference
-    elif comparator == "<":
-        return checked < reference
-    elif comparator == "<=":
-        return checked <= reference
-    else:
-        raise ValueError
+
+
+def get_comparator( comparator, threshold = 0):
+    return { "==":lambda reference, checked: abs(reference - checked) < threshold,
+            ">":lambda reference, checked: reference > checked,
+            ">=":lambda reference, checked: reference >= checked,
+            "<":lambda reference, checked: reference < checked,
+            "<=":lambda reference, checked: reference <= checked}[comparator]
+
+
 
 
 def filter_by_KB(paths, reference, comparator, threshold):
     filtered_paths = []
+    comparator = get_comparator( comparator, threshold)
     for path in paths:
-        if compare(reference, check_size_in_KB(path), comparator, threshold):
+        if comparator(reference, check_size_in_KB(path)):
             filtered_paths.append(path)
     return filtered_paths
 
