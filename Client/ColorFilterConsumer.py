4effc1c2ea3bac6a711fe8c32c742eb06ac5d810
Mexolius <39342674+Mexolius@users.noreply.github.com>
2022-05-09 21:09:54 +0200

    Implement required changes

--- Client/ColorFilterConsumer.py
+++ Client/ColorFilterConsumer.py
@@ -1,10 +1,11 @@
-from Logger.CustomLogFormatter import CustomLogFormatter
-from ColorFilter.ColorFilter import process_request
-from RabbitMq.RabbitMQClient import RabbitMQSyncConsumer, RabbitMQProducer
-from RabbitMq.Query import ResultResponse
-
-import traceback
 import logging
+import traceback
+
+from ColorFilter.ColorFilter import process_request
+from Logger.CustomLogFormatter import CustomLogFormatter
+from RabbitMq.Query import ResultResponse
+from RabbitMq.RabbitMQClient import RabbitMQSyncConsumer, RabbitMQProducer, RabbitMQAsyncConsumer
+from Utils.Utils import setup_health_consumer
 
 logger = logging.getLogger("ColorFilterConsumer")
 logger.setLevel(logging.DEBUG)
@@ -13,12 +14,19 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(CustomLogFormatter())
 logger.addHandler(ch)
 
+SERVICE_NAME = 'color_service'
+
 
 if __name__ == '__main__':
     logger.info("Starting ColorFilterConsumer")
     consumer = RabbitMQSyncConsumer.from_config('colors')
     producer = RabbitMQProducer.from_config()
+    health_consumer = RabbitMQAsyncConsumer.from_config('health')
     logger.info("ColorFilterConsumer started successfully")
+
+    logger.info("Starting HealthConsumer")
+    setup_health_consumer(SERVICE_NAME, producer, health_consumer)
+    logger.info("HealthConsumer started successfully")
 
 
     def callback(ch, method, properties, body):
@@ -28,7 +36,7 @@ if __name__ == '__main__':
             producer.publish_rmq_message(result)
         except Exception as e:
             logging.error(traceback.format_exc())
-            resp = ResultResponse(500, [], 'color_service')
+            resp = ResultResponse(500, [], SERVICE_NAME)
             producer.publish_rmq_message(resp)
 
     consumer.consume(callback)
--- Client/Utils/Utils.py
+++ Client/Utils/Utils.py
@@ -1,4 +1,7 @@
+import json
 from typing import Callable, Union
+
+from RabbitMq.Query import HealthResponseMessage
 
 Comparable = Union[float, int]
 
@@ -9,3 +12,14 @@ def get_comparator(comparator: str, threshold=0) -> Callable[[Comparable, Compar
             ">=": lambda checked, reference: checked >= reference,
             "<": lambda checked, reference: checked < reference,
             "<=": lambda checked, reference: checked <= reference}[comparator]
+
+
+def setup_health_consumer(service_name, producer, health_consumer):
+    health_message = HealthResponseMessage(service_name)
+
+    def health_callback(ch, method, properties, body):
+        if json.loads(body)['service_name'] == service_name:
+            producer.publish_rmq_message(health_message)
+            health_consumer.ack(method.delivery_tag)
+
+    health_consumer.consume(health_callback)
