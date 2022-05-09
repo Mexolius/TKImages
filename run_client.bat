d2ef0ff82d80ccb0e094315091f034568506f00a
Krzysztof <kczech123kczech123@gmail.com>
2022-05-09 16:31:42 +0200

    windows script

--- run_client.bat
+++ run_client.bat
@@ -3,9 +3,9 @@ cd ImageFinderRabbitMqSetupApp
 docker-compose up -d
 call sbt run 
 cd ../Client
-py -m pip install -r requirements.txt
-start cmd /k py ColorFilterConsumer.py 
-start cmd /k py DogFilterConsumer.py 
-start cmd /k py SimilarityConsumer.py 
-start cmd /k py SizeFilterConsumer.py 
-start cmd /k py App.py 
+python -m pip install -r requirements.txt
+start cmd /k python ColorFilterConsumer.py 
+start cmd /k python DogFilterConsumer.py 
+start cmd /k python SimilarityConsumer.py 
+start cmd /k python SizeFilterConsumer.py 
+start cmd /k python App.py 
