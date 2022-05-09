3c7b077c5b370fa1b301da7a3f1159ac4c447ec9
Krzysztof <kczech123kczech123@gmail.com>
2022-05-09 12:31:24 +0200

    fix

--- run_app.py
+++ run_app.py
@@ -2,6 +2,7 @@ from sys import platform
 from subprocess import call
 
 if platform == "linux" or platform == "linux32":
+    call(["chmod","+x","run_client.sh"])
     call("./run_client.sh")
 elif platform == "win32":
     call("run_client.bat")
